2823f6b81433882b06a79020c7d5bf510290fde7
Simon Toens <simontoens@gmail.com>
2013-03-31 20:52:31 -0700

    Add image reflection (from http://developer.apple.com/library/ios/#samplecode/Reflection/Introduction/Intro.html).

---
+++ Source/UIImageView+Reflection.h
@@ -0,0 +1,9 @@
+// @author http://developer.apple.com/library/ios/#samplecode/Reflection/Introduction/Intro.html
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (Reflection)
+
+- (UIImage *)reflectedImageWithHeight:(NSUInteger)height;
+
+@end
